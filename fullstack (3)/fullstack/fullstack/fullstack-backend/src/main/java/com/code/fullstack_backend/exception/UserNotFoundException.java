ab88d6f182652ec66228920d0b875467eebb4327
VSBERINATH <vsberinath@gmail.com>
2026-03-05 14:54:16 +0530

    removed

--- fullstack (3)/fullstack/fullstack/fullstack-backend/src/main/java/com/code/fullstack_backend/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.code.fullstack_backend.exception;
-
-public class UserNotFoundException  extends  RuntimeException{
-
-    public UserNotFoundException(Long id){
-        super("could not found user ID:"+id);
-    }
-}
--- fullstack (3)/fullstack/fullstack/fullstack-backend/src/main/java/com/code/fullstack_backend/repository/UserAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.code.fullstack_backend.repository;
-
-import com.code.fullstack_backend.model.UserAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress,Long> {
-}
